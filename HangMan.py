356ad75776c2fb52cc619ac09cbe9cb378dd1940
Casey.Mezera <casey.mezera@monsanto.com>
2016-12-20 10:18:51 -0600

    Second Game

---
+++ HangMan.py
@@ -0,0 +1,28 @@
+
+
+
+def head():
+    print("______")
+    print("|")
+    print("|    ( )")
+def body1():
+    print("|   \ | /")
+def body2():
+    print("|    \|/")
+def body3():
+    print("|     |  ")
+def leg1():
+    print("|    / \ ")
+def leg2():
+    print("|  _/   \_")
+def pole():
+    print("|")
+
+
+head()
+body1()
+body2()
+body3()
+leg1()
+leg2()
+
